8af0042cf19bb440742291d10f52677dda2939aa
ddemille1 <ddemille1@gmail.com>
2022-02-01 22:09:31 -0500

    added picture class

---
+++ picture.py
@@ -0,0 +1,51 @@
+class Picture:   
+    def draw_zero_wrong(self):
+        print('  _____  ')
+        print(' /_____\\')
+        print(' \\     /')
+        print('  \\   /')
+        print('    O')
+        print('   /|\\')
+        print('   / \\')
+        print('')
+        print('^^^^^^^^^')
+
+    def draw_one_wrong(self):
+        print(' /_____\\')
+        print(' \\     /')
+        print('  \\   /')
+        print('    O')
+        print('   /|\\')
+        print('   / \\')
+        print('')
+        print('^^^^^^^^^')
+    def draw_two_wrong(self):
+        print(' \\     /')
+        print('  \\   /')
+        print('    O')
+        print('   /|\\')
+        print('   / \\')
+        print('')
+        print('^^^^^^^^^')
+    def draw_three_wrong(self):
+        print('  \\   /')
+        print('    O')
+        print('   /|\\')
+        print('   / \\')
+        print('')
+        print('^^^^^^^^^')
+    def draw_four_wrong(self):
+        print('    X')
+        print('   /|\\')
+        print('   / \\')
+        print('')
+        print('^^^^^^^^^')
+        
+        
+#for testing:
+# jumper = Picture()
+# jumper.draw_zero_wrong()
+# jumper.draw_one_wrong()
+# jumper.draw_two_wrong()
+# jumper.draw_three_wrong()
+# jumper.draw_four_wrong()
